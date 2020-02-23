8ce73c3df5c6ab4f21819cb546cefe736924128a
JoëlPierreDupont <36246708+joelpierredupont@users.noreply.github.com>
2020-02-23 19:06:00 +0100

    Add files via upload

---
+++ lettre_elise.ps1
@@ -0,0 +1,172 @@
+﻿  start-sleep -milli 60
+  [console]::Beep(329.63, 300)
+  start-sleep -milli 35
+  [console]::Beep(311.13, 300)
+  start-sleep -milli 35
+  [console]::Beep(329.63, 300)
+  start-sleep -milli 35
+  [console]::Beep(311.13, 300)
+  start-sleep -milli 35
+  [console]::Beep(329.63, 300)
+  start-sleep -milli 35
+  [console]::Beep(246.94, 300)
+  start-sleep -milli 40
+  [console]::Beep(293.66,300)
+  start-sleep -milli 40
+  [console]::Beep(261.63,300)
+  start-sleep -milli 40
+  [console]::Beep(220, 900)
+  start-sleep -milli 100
+  [console]::Beep(146.83, 300)
+  start-sleep -milli 35
+  [console]::Beep(174.61, 300)
+  start-sleep -milli 40
+  [console]::Beep(220, 300)
+  start-sleep -milli 40
+  [console]::Beep(246.94, 900)
+  start-sleep -milli 100
+  [console]::Beep(174.61, 300)
+  start-sleep -milli 40
+  [console]::Beep(233.08, 300)
+  start-sleep -milli 40
+  [console]::Beep(246.94, 300)
+  start-sleep -milli 40
+  [console]::Beep(261.63, 900)
+  start-sleep -milli 100
+  start-sleep -milli 30
+  [console]::Beep(329.63, 300)
+  start-sleep -milli 40
+  [console]::Beep(311.13, 300)
+  start-sleep -milli 40
+  [console]::Beep(329.63, 300)
+  start-sleep -milli 40
+  [console]::Beep(311.13, 300)
+  start-sleep -milli 40
+  [console]::Beep(329.63, 300)
+  start-sleep -milli 40
+  [console]::Beep(246.94, 300)
+  start-sleep -milli 40
+  [console]::Beep(293.66, 300)
+  start-sleep -milli 40
+  [console]::Beep(261.63, 300)
+  start-sleep -milli 40
+  [console]::Beep(220, 900)
+  start-sleep -milli 100
+  [console]::Beep(146.83, 300)
+  start-sleep -milli 40
+  [console]::Beep(174.61, 300)
+  start-sleep -milli 40
+  [console]::Beep(220, 300)
+  start-sleep -milli 40
+  [console]::Beep(246.94, 900)
+  start-sleep -milli 100
+  [console]::Beep(174.61, 300)
+  start-sleep -milli 40
+  [console]::Beep(261.63, 300)
+  start-sleep -milli 40
+  [console]::Beep(246.94, 300)
+  start-sleep -milli 40
+  [console]::Beep(220, 900)
+  start-sleep -milli 100
+  [console]::Beep(246.94, 300)
+  start-sleep -milli 40
+  [console]::Beep(261.63, 300)
+  start-sleep -milli 40
+  [console]::Beep(293.66, 300)
+  start-sleep -milli 40
+  [console]::Beep(329.63, 900)
+  start-sleep -milli 100
+  [console]::Beep(196, 300)
+  start-sleep -milli 40
+  [console]::Beep(349.23, 300)
+  start-sleep -milli 40
+  [console]::Beep(329.23, 300)
+  start-sleep -milli 40
+  [console]::Beep(293.63, 900)
+  start-sleep -milli 100
+  [console]::Beep(164.81, 300)
+  start-sleep -milli 40
+  [console]::Beep(329.63, 300)
+  start-sleep -milli 40
+  [console]::Beep(293.63, 300)
+  start-sleep -milli 40
+  [console]::Beep(261.63, 900)
+  start-sleep -milli 100
+  [console]::Beep(146.83, 300)
+  start-sleep -milli 40
+  [console]::Beep(293.63, 300)
+  start-sleep -milli 40
+  [console]::Beep(261.63, 300)
+  start-sleep -milli 40
+  [console]::Beep(246.94, 900)
+  start-sleep -milli 100
+  start-sleep -milli 40
+  [console]::Beep(329.63, 300)
+  start-sleep -milli 40
+  [console]::Beep(311.13, 300)
+  start-sleep -milli 35
+  [console]::Beep(329.63, 300)
+  start-sleep -milli 35
+  [console]::Beep(311.13, 300)
+  start-sleep -milli 35
+  [console]::Beep(329.63, 300)
+  start-sleep -milli 35
+  [console]::Beep(246.94, 300)
+  start-sleep -milli 40
+  [console]::Beep(293.66,300)
+  start-sleep -milli 40
+  [console]::Beep(261.63,300)
+  start-sleep -milli 40
+  [console]::Beep(220, 900)
+  start-sleep -milli 100
+  [console]::Beep(146.83, 300)
+  start-sleep -milli 35
+  [console]::Beep(174.61, 300)
+  start-sleep -milli 40
+  [console]::Beep(220, 300)
+  start-sleep -milli 40
+  [console]::Beep(246.94, 900)
+  start-sleep -milli 100
+  [console]::Beep(174.61, 300)
+  start-sleep -milli 40
+  [console]::Beep(233.08, 300)
+  start-sleep -milli 40
+  [console]::Beep(246.94, 300)
+  start-sleep -milli 40
+  [console]::Beep(261.63, 900)
+  start-sleep -milli 100
+  start-sleep -milli 30
+  [console]::Beep(329.63, 300)
+  start-sleep -milli 40
+  [console]::Beep(311.13, 300)
+  start-sleep -milli 40
+  [console]::Beep(329.63, 300)
+  start-sleep -milli 40
+  [console]::Beep(311.13, 300)
+  start-sleep -milli 40
+  [console]::Beep(329.63, 300)
+  start-sleep -milli 40
+  [console]::Beep(246.94, 300)
+  start-sleep -milli 40
+  [console]::Beep(293.66, 300)
+  start-sleep -milli 40
+  [console]::Beep(261.63, 300)
+  start-sleep -milli 40
+  [console]::Beep(220, 900)
+  start-sleep -milli 100
+  [console]::Beep(146.83, 300)
+  start-sleep -milli 40
+  [console]::Beep(174.61, 300)
+  start-sleep -milli 40
+  [console]::Beep(220, 300)
+  start-sleep -milli 40
+  [console]::Beep(246.94, 900)
+  start-sleep -milli 100
+  [console]::Beep(174.61, 300)
+  start-sleep -milli 40
+  [console]::Beep(261.63, 300)
+  start-sleep -milli 40
+  [console]::Beep(246.94, 300)
+  start-sleep -milli 40
+  [console]::Beep(220, 900)
+  start-sleep -milli 600
